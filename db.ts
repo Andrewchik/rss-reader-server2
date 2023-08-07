247c27c43ce48b16ed49713e74a962a5995aa0e0
Andrew Goryachiy <42047089+Andrewchik@users.noreply.github.com>
2023-08-07 23:43:08 +0300

    [UPD] env

--- db.ts
+++ db.ts
@@ -1,9 +1,18 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://andrew:qwerty123@cluster0.hvhfelv.mongodb.net/?retryWrites=true&w=majority');
-    console.log('Connected to MongoDB');
+    const mongodbUri = process.env.MONGODB_URI;
+
+    if (!mongodbUri) {
+      console.error('MONGODB_URI environment variable is not set');
+    } else {
+      await mongoose.connect(mongodbUri);
+      console.log('Connected to MongoDB');
+    }
+  
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
   }
